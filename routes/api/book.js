25961f23b18ac0fe7e731bf48d78fdf0aded2cc0
Bhavika Patel <bhavij28@gmail.com>
2021-06-08 15:18:21 -0400

    updated Books to books

--- routes/api/book.js
+++ routes/api/book.js
@@ -1,12 +1,12 @@
 const router = require("express").Router();
 const booksController = require("../../controllers/savedBooks");
 
-// Matches with "/api/books"
+
 router.route("/")
   .get(booksController.findAll)
   .post(booksController.create);
 
-// Matches with "/api/books/:id"
+
 router
   .route("/:id")
   .get(booksController.findById)
@@ -14,3 +14,4 @@ router
   .delete(booksController.remove);
 
 module.exports = router;
+
